f41a7b9b31665fafa65cd1d50757ebd7a542cc4d
Yonis MAHADE <ymahade@e2r2p16.42.fr>
2016-08-09 12:52:15 +0200

    save

--- j02/ex03/ft_print_comb.c
+++ j02/ex03/ft_print_comb.c
@@ -5,28 +5,30 @@ void	ft_putchar(char c)
 	write (1, &c, 1);
 }
 
-void	ft_print_putchar(void)
-{:
-	if (c = 7)
+void	ft_print_putchar(char c, char d, char u)
+{
+	if (c < '7')
 	{ 
-		ft_putchar(c);
-		ft_putchar(d);
-		ft_putchar(u);
-	}
-	else
-	{
 		ft_putchar(c);
 		ft_putchar(d);
 		ft_putchar(u);
 		ft_putchar(',');
 		ft_putchar(' ');
 	}
+	else
+	{
+		ft_putchar(c);
+		ft_putchar(d);
+		ft_putchar(u);
+	}
+}
 
 void 	ft_print_comb(void)
 {
 	char c;
 	char d;
 	char u;
+
 	c = '0';
 	while (c <= '7')
 	{
@@ -36,7 +38,7 @@ void 	ft_print_comb(void)
 			u = d + 1;
 			while (u <= '9')
 			{
-				ft_print_putchar();
+				ft_print_putchar(c, d, u);
 				u++;
 			}
 			d++;
--- j02/ft_print_comb2.c
+++ j02/ft_print_comb2.c
@@ -5,6 +5,23 @@ void	ft_putchar(char c)
 	write (1, &c, 1);
 }
 
+void	ft_putchars(c, d, u)
+{
+	if (c = 7)
+	{
+		ft_putchar(c);
+		ft_putchar(d);
+		ft_putchar(u);
+	}
+	else
+	{
+		ft_putchar(c);
+		ft_putchar(d);
+		ft_putchar(u);
+		ft_putchar(',')
+		ft_pucthar(' ')
+	}
+
 void 	ft_print_comb(void)
 {
 	char c;
@@ -19,11 +36,7 @@ void 	ft_print_comb(void)
 			u = d + 1;
 			while (u <= '9')
 			{
-				ft_putchar(c);
-				ft_putchar(d);
-				ft_putchar(u);
-				ft_putchar(',');
-				ft_putchar(' ');
+				ft_putchars(c, d, u);
 				u++;
 			}
 			d++;
